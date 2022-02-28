339b53b6f374518302f70febc051cc4b10cd13f5
Millennium Cyborg <y2k.cyborg@hotmail.com>
2022-02-28 15:07:49 +0000

    Persist config

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,31 +1,117 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Timers;
 using UnityEngine;
 
 [RequireComponent(typeof(uOSC.uOscClient))]
 public class App : MonoBehaviour
 {
+    // Public instance references
     public UnityEngine.UI.Slider slider;
     public UnityEngine.UI.Text info;
     public UnityEngine.UI.InputField port_input;
     public UnityEngine.UI.InputField blendshape_input;
 
-    uOSC.uOscClient client;
+    // Internal types
+    [Serializable]
+    public struct Channel
+    {
+        public string name;
+        public float value;
+        // public float min;
+        // public float max;
+    }
 
-    // Use this for initialization
+    [Serializable]
+    public struct Config
+    {
+        // public string addr;
+        public int port;
+        public Channel[] channels;
+    }
+
+    // Private implementation
+    private uOSC.uOscClient client;
+    private Config config;
+    private string save_path;
+    private Timer save_timer;
+
+    // Initialization
     void Start()
     {
         client = GetComponent<uOSC.uOscClient>();
+
+        save_path = Path.Combine(Application.persistentDataPath, "config.json");
+
+        if (!TryLoadConfig(ref config)) {
+            config = CreateDefaultConfig();
+        }
+
+        UpdateUIFromConfig();
+
+        save_timer = new Timer(15000);
+        save_timer.Elapsed += OnSaveTimerElapsed;
+        save_timer.AutoReset = true;
+        save_timer.Enabled = true;
+    }
+    
+    static Config CreateDefaultConfig() {
+        Config new_config = new Config();
+        new_config.port = 39540;
+        new_config.channels = new Channel[1];
+        new_config.channels[0].name = "JOY";
+        new_config.channels[0].value = 0;
+        return new_config;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        int port_maybe = 0;
-        if (int.TryParse(port_input.text, out port_maybe)) {
-          client.port = port_maybe;
+    bool TryLoadConfig(ref Config c) {
+        try {
+            string jsonString = null;
+            using (StreamReader reader = new StreamReader(save_path)) {
+                jsonString = reader.ReadToEnd();
+            }
+            Config loaded = JsonUtility.FromJson<Config>(jsonString);
+            c = loaded;
+            Debug.Log("Config loaded.");
+            return true;
+        } catch (Exception e) {
+            Debug.LogWarning("Couldn't load config file: " + e.Message);
+            return false;
         }
-        info.text = "Sending value to blendshape '" + blendshape_input.text + "' on port " + client.port;
+    }
+    
+    void WriteConfig(Config config) {
+        string jsonString = JsonUtility.ToJson(config);
+        using (StreamWriter writer = new StreamWriter(save_path)) {
+            writer.Write(jsonString);
+        }
+    }
+
+    private void OnSaveTimerElapsed(System.Object obj, ElapsedEventArgs e) {
+        WriteConfig(config);
+    }
+
+    void UpdateConfigFromUI() {
+        int port = 0;
+        if (int.TryParse(port_input.text, out port)) {
+            config.port = port;
+        }
+
+        config.channels[0].name = blendshape_input.text;
+        config.channels[0].value = slider.value;
+    }
+
+    void UpdateUIFromConfig() {
+        info.text = "Sending value to blendshape '" + config.channels[0].name + "' on port " + config.port;
+        blendshape_input.text = config.channels[0].name;
+        slider.value = config.channels[0].value;
+        port_input.text = config.port.ToString();
+    }
+
+    void VMCSend() {
+        client.port = config.port;
 
         /* Google Translation of documentation:
         Frame Period
@@ -59,8 +145,22 @@ public class App : MonoBehaviour
 
         BlendShapeProxy value
         */
-        client.Send("/VMC/Ext/Blend/Val", blendshape_input.text, slider.value);
+        // I'm assuming we can send several blendshapes and apply all at once.
+        foreach (Channel c in config.channels) {
+            client.Send("/VMC/Ext/Blend/Val", config.channels[0].name, config.channels[0].value);
+        }
         client.Send("/VMC/Ext/Blend/Apply");
+    }
+
+    void Update()
+    {
+        UpdateConfigFromUI();
+        UpdateUIFromConfig();
+        VMCSend();
+    }
+
+    private void OnApplicationQuit() {
+        WriteConfig(config);
     }
 }
 
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # VMC Blendshape Slider
 
+## Info
+
 This is a quick prototype. It sends the value 0-1 of the slider over VMC for the specified blendshape on the specified port.
 
 If you want to build it, I'm using Unity 2019.4.21f1.
 
 I'm using https://github.com/hecomi/uOSC/releases to send, idk how to properly package stuff in Unity but I don't think you would need to install it yourself.
+
+Let me know if you use it, if you have an idea for a feature or if you find a bug! I can't easily test a Mac or Linux build but let me know if you'd find one useful.
 
 ## How to use
 
@@ -15,3 +19,15 @@ To properly combine this with VSeeFace tracking:
 - Enable 'Apply VSeeFace tracking' below that and make sure everything is enabled
 - Enable 'Secondary OSC/VMC protocol receiver'. This is where we send the slider value, so the port number should correspond to the one in the slider app.
 - Enable 'Apply Blendshapes' beneath that.
+
+## Version History
+
+v0.2 - Settings are now saved on exit.
+
+v0.1 - Initial Release.
+
+## Contact
+
+https://y2kcyb.org
+https://twitter.com/y2k_cyborg
+https://twitch.tv/millennium_cyborg
